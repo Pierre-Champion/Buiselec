82722b3fc086b0a0f06c5d467bb2c57ac434977d
Pierre-Champion <pierre.champion@bbox.fr>
2018-06-05 14:04:43 +0200

    Version 1.0

---
+++ application/controllers/Visiteur.php
@@ -0,0 +1,25 @@
+<?php
+class Visiteur extends CI_Controller 
+{
+   public function __construct()
+   {
+      parent::__construct();
+      $this->load->helper('url');
+      $this->load->helper('assets');
+   } // __construct
+
+   public function Home() {
+    $DonneesInjectees['TitreDeLaPage'] = "Page d'acceuil";
+    $config["base_url"] = site_url('visiteur/Home');  
+    $this->load->view('templates/Entete');
+    $this->load->view("visiteur/Home", $DonneesInjectees);
+    $this->load->view('templates/PiedDePage');
+   }// Page d'acceuil
+
+   public function Contact() {
+    $DonneesInjectees['TitreDeLaPage'] = "Contact";
+    $this->load->view('templates/Entete');
+    $this->load->view("visiteur/Contact", $DonneesInjectees);
+    $this->load->view('templates/PiedDePage');
+   }// Page d'acceuil
+}
---
+++ application/views/templates/Entete.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Buiselec</title>
+</head>
+<body>
+<a href="<?php echo site_url('visiteur/Home') ?>">Page d'acceuil</a>&nbsp;&nbsp;
+<a href="<?php echo site_url('visiteur/Contact') ?>">Contactez-nous</a>&nbsp;&nbsp;
+</body>
