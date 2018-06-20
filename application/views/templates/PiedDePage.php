f71a31e12237a76e95e29fdb383edad467109355
KenseoShow <parisbqd@gmail.com>
2018-06-20 17:06:48 +0200

    version 2.013

--- application/views/templates/PiedDePage.php
+++ application/views/templates/PiedDePage.php
@@ -11,4 +11,17 @@
         Ok
     </div>
 </div>
+<div class="sharer">
+<div id="Facebook">
+            <iframe src="http://www.facebook.com/plugins/like.php?href=https://www.facebook.com/pages/Buiselec/134086977128833&layout=box_count&show_faces=true&width=65&action=like&font=arial&colorscheme=light&height=65" 
+            scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:65px; margin-top:3px;" allowTransparency="true"></iframe>
+</div>
+            <div id="Youtube">
+            <script src="https://apis.google.com/js/platform.js"></script>
+            <div class="g-ytsubscribe" data-channel="Buiselec" data-layout="full" data-theme="dark" data-count="default"></div>
+            </div>
+            
+       </div>
+</div>
+
 </body>
